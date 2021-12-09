6a2731bb539b83a9f6f9da621f461eb2a5a7bb81
Wendell Liu <cuk.bas@gmail.com>
2021-12-09 23:17:34 +0800

    fix: custom callback

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,75 +1,90 @@
 // 引用 Express 與 Express 路由器
-const express = require('express')
-const router = express.Router()
-const User = require('../../models/user')
-const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/user");
+const passport = require("passport");
 
 // routes
-router.get('/login', (req, res) => {
-  res.render('login')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login',
-  failureFlash: true
-}),
-  (req, res) => {
-    res.render('/users/login', { messages: req.flash('error') })
-  }
-)
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      console.log("post /users/login", "has err");
+      return res.render("login", {
+        messages: req.flash("error")
+      });
+    }
+    if (!user) {
+      console.log("post /users/login", "no user");
+      return res.render("login", {
+        messages: req.flash("error")
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        console.log("post /users/login", "has err in log in");
+        return res.render("login", {
+          messages: req.flash("error")
+        });
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
 
-router.get('/register', (req, res) => {
-  res.render('register')
-})
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
   // check error
-  const error = []
+  const error = [];
   if (!name || !email || !password || !confirmPassword) {
-    error.push({ message: '所有欄位都是必須填寫的!' })
+    error.push({ message: "所有欄位都是必須填寫的!" });
   }
   if (password !== confirmPassword) {
-    error.push({ message: '密碼與確認密碼不符!' })
+    error.push({ message: "密碼與確認密碼不符!" });
   }
   if (error.length) {
-    return res.render('register', {
+    return res.render("register", {
       error,
       name,
       email,
       password,
       confirmPassword
-    })
+    });
   }
   // find existed email
   User.findOne({ email }).then(user => {
     if (user) {
-      error.push({ message: '這個 Email 已經註冊過了!' })
-      return res.render('register', {
+      error.push({ message: "這個 Email 已經註冊過了!" });
+      return res.render("register", {
         error,
         name,
         email,
         password,
         confirmPassword
-      })
+      });
     } else {
       return User.create({
         name,
         email,
         password
       })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
+        .then(() => res.redirect("/"))
+        .catch(err => console.log(err));
     }
+  });
+});
 
-  })
-})
-
-router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', '你已經成功登出。')
-  res.redirect('/users/login')
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "你已經成功登出。");
+  res.redirect("/users/login");
+});
 // 匯出路由模組
-module.exports = router
+module.exports = router;
